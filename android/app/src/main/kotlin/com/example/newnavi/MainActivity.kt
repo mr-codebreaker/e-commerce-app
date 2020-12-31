7878f590219b760b8301035d30d1bbaa6cb9d181
Dilshad <dilshadhabib28@gmail.com>
2020-12-31 12:29:48 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/newnavi/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.newnavi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp
+    (
+new MaterialApp
+    (
+    home:new Center
+      (
+        child: new Text("HelLo World"),
+           )
+        )
+    );
+
+}
